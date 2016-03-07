bc802dd710138561cecc1401b7853fea96cded4b
Esko Haila <eracage@gmail.com>
2016-03-08 00:35:32 +0200

    centralized colors, added spinning to requested time istead of instant transition

--- Project_08_02_2016/clockwidget.cpp
+++ Project_08_02_2016/clockwidget.cpp
@@ -3,30 +3,31 @@
 #include <QVector2D>
 #include <QPolygon>
 #include <QTimerEvent>
-#include <QStyle>
+#include <QPalette>
 
 ClockWidget::ClockWidget(QWidget *parent) : QWidget(parent)
 {
     m_timerId = startTimer(50);
+    dateTime = QDateTime::currentDateTime();
     systime = true;
 }
 
 void ClockWidget::setTime(QTime time)
 {
     systime = false;
-    dateTime.setTime(time);
+    targetTime.setTime(time);
 }
 
 void ClockWidget::setDate(QDate date)
 {
     systime = false;
-    dateTime.setDate(date);
+    targetTime.setDate(date);
 }
 
 void ClockWidget::setDateTime(QDateTime dateT)
 {
     systime = false;
-    dateTime = dateT;
+    targetTime = dateT;
 }
 
 void ClockWidget::setCurTime()
@@ -36,6 +37,15 @@ void ClockWidget::setCurTime()
 
 void ClockWidget::paintEvent(QPaintEvent *)
 {
+    QColor CBackground( palette().window().color());
+    QColor CCircle(     palette().windowText().color());
+    QColor CHMarkers(   Qt::red);//palette().dark().color());
+    QColor CMMarkers(   palette().dark().color());
+    QColor CHHand(      128,0,128);//palette().dark().color());
+    QColor CMHand(      0,128,128);//palette().mid().color());
+    QColor CSHand(      128,128,0);//palette().shadow().color());
+    QColor CText(       palette().text().color());
+
     QPointF center((width())/2.0f, (height())/2.0f);
     float clockSize = height() > width() ? width()-3 : height()-3;
     if (clockSize <= 0)
@@ -43,6 +53,9 @@ void ClockWidget::paintEvent(QPaintEvent *)
 
     QPainter painter(this);
     QPen pen;
+    QFont font("Helvetica");
+
+    painter.setBackground(QBrush(CBackground));
 
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setRenderHint(QPainter::HighQualityAntialiasing);
@@ -50,18 +63,25 @@ void ClockWidget::paintEvent(QPaintEvent *)
     painter.translate(center);
 
     if (systime)
-        dateTime = QDateTime::currentDateTime();
+        targetTime = QDateTime::currentDateTime();
+
+    dateTime = dateTime.addSecs(qRound(dateTime.secsTo(targetTime) * 0.5));
+    dateTime = dateTime.addMSecs(qRound(dateTime.msecsTo(targetTime) * 0.5));
 
     QTime time = dateTime.time();
 
     pen.setCapStyle(Qt::FlatCap);
 
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(CBackground);
+    painter.drawEllipse(QPointF(),clockSize * 0.485f, clockSize * 0.485f);
+    painter.setBrush(Qt::NoBrush);
 
-    pen.setColor(palette().mid().color());
-    pen.setWidthF(clockSize*0.01);
-    painter.setPen(pen);
 
     painter.save();
+    pen.setColor(CMMarkers);
+    pen.setWidthF(clockSize*0.01);
+    painter.setPen(pen);
     for (int i = 0; i < 60; i++)
     {
         painter.drawLine(QPointF(0, clockSize * 0.46f),QPoint(0, clockSize * 0.485f));
@@ -69,12 +89,11 @@ void ClockWidget::paintEvent(QPaintEvent *)
     }
     painter.restore();
 
-    palette().
-    pen.setColor(QColor(255,0,0));
-    pen.setWidthF(clockSize*0.02);
-    painter.setPen(pen);
 
     painter.save();
+    pen.setColor(CHMarkers);
+    pen.setWidthF(clockSize*0.02);
+    painter.setPen(pen);
     for (int i = 0; i < 12; i++)
     {
         painter.drawLine(QPointF(0, clockSize * 0.44f),QPoint(0, clockSize * 0.485f));
@@ -82,17 +101,13 @@ void ClockWidget::paintEvent(QPaintEvent *)
     }
     painter.restore();
 
-    pen.setColor(QColor(0,0,0));
+    pen.setColor(CCircle);
     pen.setWidthF(clockSize*0.01);
     painter.setPen(pen);
-
     painter.drawEllipse(QPointF(),clockSize * 0.485f, clockSize * 0.485f);
 
-
-    QFont font("Helvetica");
-    font.setPointSizeF(clockSize * 0.05f);
-    painter.setFont(font);
-    //pen.setWidthF(clockSize*0.05f);
+    pen.setColor(CText);
+    painter.setPen(pen);
 
     QString weekday = "";
     switch (dateTime.date().dayOfWeek())
@@ -115,6 +130,8 @@ void ClockWidget::paintEvent(QPaintEvent *)
         break;
     }
 
+    font.setPointSizeF(clockSize * 0.05f);
+    painter.setFont(font);
     painter.drawText(
                 -clockSize*0.35f,
                 -clockSize*0.3f,
@@ -125,7 +142,6 @@ void ClockWidget::paintEvent(QPaintEvent *)
 
     font.setPointSizeF(clockSize * 0.15f);
     painter.setFont(font);
-
     painter.drawText(
                 -clockSize*0.3f,
                 -clockSize*0.2f,
@@ -133,7 +149,6 @@ void ClockWidget::paintEvent(QPaintEvent *)
                 clockSize*0.25f,
                 Qt::AlignCenter,
                 QString::number(dateTime.date().day()));
-
 
     painter.drawText(
                 clockSize*0.05f,
@@ -145,7 +160,6 @@ void ClockWidget::paintEvent(QPaintEvent *)
 
     font.setPointSizeF(clockSize * 0.10f);
     painter.setFont(font);
-
     painter.drawText(
                 -clockSize*0.3f,
                 clockSize*0.05f,
@@ -154,10 +168,7 @@ void ClockWidget::paintEvent(QPaintEvent *)
                 Qt::AlignCenter,
                 QString::number(dateTime.date().year()));
 
-
     painter.setPen(Qt::NoPen);
-
-    painter.setBrush(QColor(128,0,128));
 
     const QPoint hourHand[4] =
     {
@@ -168,6 +179,7 @@ void ClockWidget::paintEvent(QPaintEvent *)
     };
 
     painter.save();
+    painter.setBrush(CHHand);
     painter.rotate((time.hour() + time.minute()/60.0f) * (360/12));
     painter.drawConvexPolygon(hourHand, 4);
     painter.drawPie(-clockSize*0.035f,
@@ -179,7 +191,6 @@ void ClockWidget::paintEvent(QPaintEvent *)
                 );
     painter.restore();
 
-    painter.setBrush(QColor(0,128,128));
 
     const QPoint minuteHand[4] =
     {
@@ -190,6 +201,7 @@ void ClockWidget::paintEvent(QPaintEvent *)
     };
 
     painter.save();
+    painter.setBrush(CMHand);
     painter.rotate((time.minute() + time.second()/60.0f) * (360/60));
     painter.drawConvexPolygon(minuteHand, 4);
     painter.drawPie(-clockSize*0.03f,
@@ -201,8 +213,6 @@ void ClockWidget::paintEvent(QPaintEvent *)
                 );
     painter.restore();
 
-    painter.setBrush(QColor(128,128,0));
-
     const QPoint secondHand[4] =
     {
         QPoint(0,clockSize*0.02f),
@@ -212,6 +222,7 @@ void ClockWidget::paintEvent(QPaintEvent *)
     };
 
     painter.save();
+    painter.setBrush(CSHand);
     painter.rotate((time.second() + time.msec()/1000.0f) * (360/60));
     painter.drawConvexPolygon(secondHand, 4);
     painter.drawPie(-clockSize*0.02f,
--- Project_08_02_2016/clockwidget.h
+++ Project_08_02_2016/clockwidget.h
@@ -22,6 +22,7 @@ protected:
 private:
     int m_timerId;
     QDateTime dateTime;
+    QDateTime targetTime;
     bool systime;
 
 signals:
